80b662a7b8ae00a7ead02e16fe58238a41221e5d
Marcin Piczkowski <marcin.piczkowski@aurea.com>
2017-10-13 02:11:43 +0200

    tomcat startup check improvement

--- wait_until_deployed.sh
+++ wait_until_deployed.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
 echo $1
-until [ "`curl --silent --show-error --connect-timeout 1 -I ${1} | grep 'HTTP/1.1 200'`" != "" ];
+# max wait retry * sleep = 300s (5min)
+maxRetry=30
+retry=1
+#until [ $retry < $maxRetry ] && [ "`curl --silent --show-error --connect-timeout 1 -I ${1} | grep 'HTTP/1.1 200'`" != "" ];
+until (( $retry > $maxRetry )) || [ "`curl --silent --show-error --connect-timeout 1 -I ${1} | grep 'HTTP/1.1 200'`" != "" ];
 do
-  echo --- sleeping for 5 seconds
-  sleep 5
+  echo --- sleeping for 10 seconds
+  sleep 10
+  retry=$(($retry + 1))
 done
 
-echo Tomcat is ready!
+if (( $retry > $maxRetry )) ; then
+  echo Failed to start server
+  exit 1
+else
+  echo Server is ready!
+fi
